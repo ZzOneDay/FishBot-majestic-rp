14ab601d0e91ba0b83605963ac20dcb8f74ed1fd
Pavel Novikov <pavel.novikov@dataart.com>
2024-03-24 02:20:40 +0100

    Update cascade service

--- src/main/kotlin/com/pnovikov/parentcontrol/configuration/AppConfig.kt
+++ src/main/kotlin/com/pnovikov/parentcontrol/configuration/AppConfig.kt
@@ -1,8 +1,11 @@
 package com.pnovikov.parentcontrol.configuration
 
+import nu.pattern.OpenCV
+import org.opencv.objdetect.CascadeClassifier
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ResourceLoader
 import java.awt.AWTException
 import java.awt.Robot
 
@@ -10,10 +13,23 @@ import java.awt.Robot
 @ComponentScan("com.pnovikov.parentcontrol")
 class AppConfig {
 
+    init {
+        OpenCV.loadLocally()
+    }
+
     @Bean
     @Throws(AWTException::class)
     fun robot(): Robot {
         System.setProperty("java.awt.headless", "false")
         return Robot()
+    }
+
+    @Bean
+    fun fishHookDetection(resourceLoader: ResourceLoader): CascadeClassifier {
+        val hookDetector = CascadeClassifier()
+        val templateImagePath = resourceLoader.getResource("template/cascade.xml").file.path
+            ?: throw IllegalArgumentException("Cascade is not found")
+        hookDetector.load(templateImagePath);
+        return hookDetector
     }
 }
--- src/main/kotlin/com/pnovikov/parentcontrol/scanner/FindFishService.kt
+++ src/main/kotlin/com/pnovikov/parentcontrol/scanner/FindFishService.kt
@@ -1,55 +1,52 @@
 package com.pnovikov.parentcontrol.scanner
 
-import nu.pattern.OpenCV
 import org.opencv.core.*
 import org.opencv.imgcodecs.Imgcodecs
 import org.opencv.imgproc.Imgproc
 import org.opencv.objdetect.CascadeClassifier
-import org.springframework.core.io.ResourceLoader
 import org.springframework.stereotype.Service
-import java.awt.Color
 import java.awt.image.BufferedImage
-import java.awt.image.DataBufferByte
-import java.awt.image.DataBufferInt
-import java.awt.image.Raster
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import javax.imageio.ImageIO
 
 @Service
 class FindFishService(
-        var resourceLoader: ResourceLoader
+    val fishHookDetection: CascadeClassifier
 ) {
 
-    fun findFish(image: BufferedImage): java.awt.Point {
-        val minY = 100;
-        val minX = 100;
-        val maxY = image.height - 300
-        val maxX = image.width - 400
+    fun findFish(image: BufferedImage): java.awt.Point? {
 
-        OpenCV.loadLocally()
-        val hookDetector = CascadeClassifier()
+        // Place for searching
+        val minX = 100
+        val minY = 100
+        val width = 400
+        val height = 1400
 
-        val templateImagePath = resourceLoader.getResource("template/cascade.xml")?.file?.path
-                ?: throw IllegalArgumentException()
+        // Image to Mat
+        val cropImage = cropImage(image, minX, minY, width, height)
+        val sourceImage = bufferedImageToMat(cropImage)
 
-        val sourceImage = bufferedImageToMat(image)
-
-        // Detecting hook
+        // Detecting hook on Mat image
         val hookDetections = MatOfRect()
-        hookDetector.load(templateImagePath);
+        fishHookDetection.detectMultiScale(sourceImage, hookDetections, 1.1, 1, 1, Size(24.0, 24.0))
 
-        hookDetector.detectMultiScale(sourceImage, hookDetections, 1.1,1,1, org.opencv.core.Size(24.0, 24.0))
 
-        val hooks: List<Rect> = hookDetections.toList()
+        return hookDetections.toList().map { java.awt.Point(it.x, it.y) }.firstOrNull()
+    }
 
+    fun cropImage(image: BufferedImage, minX: Int, minY: Int, width: Int, height: Int): BufferedImage {
+        return image.getSubimage(minX, minY, width, height)
+    }
+
+    fun showResult(sourceImage: Mat, hooks: List<Rect>) {
         for (rect in hooks) {
             Imgproc.rectangle(
-                    sourceImage,
-                    org.opencv.core.Point(rect.tl().x, rect.tl().y), // top-left corner
-                    org.opencv.core.Point(rect.br().x, rect.br().y), // bottom-right corner
-                    Scalar(0.0, 255.0, 0.0), // color (green)
-                    2 // thickness of the rectangle
+                sourceImage,
+                Point(rect.tl().x, rect.tl().y), // top-left corner
+                Point(rect.br().x, rect.br().y), // bottom-right corner
+                Scalar(0.0, 255.0, 0.0), // color (green)
+                2 // thickness of the rectangle
             )
         }
 
@@ -59,8 +56,7 @@ class FindFishService(
         val byteArray = imageBytes.toArray()
         val inputStream = ByteArrayInputStream(byteArray)
         val bufferedImage = ImageIO.read(inputStream)
-
-        return java.awt.Point()
+        print("Check bufferedImage in debug")
     }
 
     fun bufferedImageToMat(bufferedImage: BufferedImage): Mat {
